774d30e84419a6c8ec014c8067ab4f0517ce462b
Ilya Kuznetsov <kuznecov95-95@yandex.ru>
2024-02-09 21:07:17 +0300

    Initial Commit

---
+++ Otus_Pro_1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Otus_Pro_1
+//
+//  Created by Илья Кузнецов on 09.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Otus_Pro_1/Otus_Pro_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Otus_Pro_1App.swift
+//  Otus_Pro_1
+//
+//  Created by Илья Кузнецов on 09.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct Otus_Pro_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
